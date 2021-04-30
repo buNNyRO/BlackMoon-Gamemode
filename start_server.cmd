8f42545dfb0a78f0f259940f9725f1cd6cec8384
buNNy <vlogsdaniel13@gmail.com>
2021-04-30 09:54:42 +0300

    Create start_server.cmd

---
+++ start_server.cmd
@@ -0,0 +1,14 @@
+@echo off
+echo --------------------------------------------------
+echo            Gamemode SA:MP Version 1.5
+echo            Scripter Vicentzo.
+echo			Ajutoare:Bunny.
+echo          Sigur vrei sa pornesti serverul?
+echo --------------------------------------------------
+echo.
+echo.
+echo.
+echo.
+pause
+start plugins\samp-server.exe
+exit
